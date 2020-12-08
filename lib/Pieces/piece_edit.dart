fa23bf23ee32c60c93db44a15664e3d781aea08d
Matteus Magnusson <senth.wallace@gmail.com>
2020-12-08 19:46:01 +0100

    ✨ New page

---
+++ lib/Pieces/piece_edit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PieceEdit extends StatefulWidget {
+  PieceEdit({Key key}) : super(key: key);
+
+  @override
+  _PieceEditState createState() => _PieceEditState();
+}
+
+class _PieceEditState extends State<PieceEdit> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
