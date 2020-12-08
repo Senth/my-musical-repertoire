08b9ae4967f9d54765a583a8e6f4585d610134b5
Matteus Magnusson <senth.wallace@gmail.com>
2020-12-08 19:46:49 +0100

    🐛 Remove cupertino reference

--- lib/App.dart
+++ lib/App.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_musical_repertoire/Pieces/piece_list.dart';
