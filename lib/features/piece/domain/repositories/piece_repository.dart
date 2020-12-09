a89683fb35b552d6e9db1a680bcd74f833e327fd
Matteus Magnusson <senth.wallace@gmail.com>
2020-12-09 13:54:22 +0100

    ✏ Document methods

--- lib/features/piece/domain/repositories/piece_repository.dart
+++ lib/features/piece/domain/repositories/piece_repository.dart
@@ -4,7 +4,15 @@ import '../../../../core/errors/error.dart';
 import '../entities/piece.dart';
 
 abstract class PieceRepository {
+  /// Add a new piece to your musical repertoire
+  /// Returns the piece with a set newly set id or an [Error] if the piece couldn't be added
   Future<Either<Error, Piece>> addPiece(Piece piece);
+
+  /// Update/Save the changed piece information
+  /// Returns the updated piece or an [Error] if the piece couldn't be updated
   Future<Either<Error, Piece>> updatePiece(Piece piece);
+
+  /// Remove a piece with [id]
+  /// Returns the [id] if the piece was successfully removed, or an [Error] if something went wrong
   Future<Either<Error, String>> removePiece(String id);
 }
