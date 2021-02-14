cdaa62415b5648d5a50d0d2ba786938b29ad2398
Matteus Magnusson <senth.wallace@gmail.com>
2021-02-14 19:01:23 +0100

    ✨ Hive implementation of piece

--- lib/core/failures/exceptions.dart
+++ lib/core/failures/exceptions.dart
@@ -1,5 +1,8 @@
+/// Thrown when Datasources has an exception
+abstract class DataSourceException implements Exception {}
+
 /// Thrown when something went wrong with thet local server
-class LocalServerException implements Exception {}
+class LocalServerException implements DataSourceException {}
 
 /// Thrown when something went wrong with a remote server
-class RemoteServerException implements Exception {}
+class RemoteServerException implements DataSourceException {}
--- test/features/piece/data/datasources/piece_local_data_source_hive_test.dart
+++ test/features/piece/data/datasources/piece_local_data_source_hive_test.dart
@@ -11,18 +11,48 @@ void main() {
     tearDown(() => HiveGateway.close());
     setUpAll(() => repo = PieceLocalDataSourceHive());
 
-    group("addPiece()", () {
-      test("should add the piece to the DB and return with inserted (with ID) when that piece does not exist", () async {
-        final testData = [
-          PieceEntity(title: "Moonlight Sonata", composer: "Beethoven"),
-          PieceEntity(title: "Ronda alla Turca", composer: "Mozart", lastPracticed: DateTime.now())
-        ];
+    test("addPiece() should add the piece to the DB and return with inserted (with ID) when that piece does not exist",
+        () async {
+      final testData = [
+        PieceEntity(title: "Moonlight Sonata", composer: "Beethoven"),
+        PieceEntity(title: "Ronda alla Turca", composer: "Mozart", lastPracticed: DateTime.now())
+      ];
+      final box = await HiveGateway.piecesBox;
 
-        for (final test in testData) {
-          final result = await repo.addPiece(test);
-          // TODO CONTINUE
-        }
-      });
+      for (final test in testData) {
+        final result = await repo.addPiece(test);
+        expect(result, test.copy(id: result.id));
+        expect(box.get(result.id), result.toJson());
+      }
+    });
+
+    test("removePiece() should remove the piece from the DB when the piece exists in the DB", () async {
+      final box = await HiveGateway.piecesBox;
+      final testData = [
+        PieceEntity(id: "1", title: "Moonlight", composer: "Beethoven"),
+        PieceEntity(id: "2", title: "Turca", composer: "Mozart"),
+      ];
+
+      for (final test in testData) {
+        await box.put(test.id, test.toJson());
+        final result = await repo.removePiece(test.id);
+        expect(result, test.id);
+      }
+    });
+
+    test("updatePiece() should update the saved piece when updating with new values", () async {
+      final box = await HiveGateway.piecesBox;
+      final testData = [
+        PieceEntity(id: "1", title: "Moonlight", composer: "Beethoven"),
+        PieceEntity(id: "1", title: "Turca", composer: "Mozart"),
+        PieceEntity(id: "1", title: "Military March", composer: "Schubert"),
+      ];
+
+      for (final test in testData) {
+        final result = await repo.updatePiece(test);
+        expect(result, test);
+        expect(result, PieceEntity.fromJson(await box.get(test.id)));
+      }
     });
   });
 }
